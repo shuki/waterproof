7a6e4997889b99b39cc7f8e15b04f3398417b378
Shuki Shukrun <shukrun.shuki@gmail.com>
2018-01-22 10:58:30 +1300

    jset.php handle export.

--- jset/server/jset.php
+++ jset/server/jset.php
@@ -7,6 +7,13 @@ $result = jset_dispatch::get($request);
 if($request['_methods_'] == 'export'){
 	header('Content-disposition: attachment; filename=' . $request['_source_'] . '.csv');
 	header('Content-type: text/csv');
+	echo substr($result, 0, -2);
+	exit;
+}
+
+if($request['_file_output_']){
+	header('Content-disposition: attachment; filename=file.txt');
+	header('Content-type: text/csv');
 }
 
 echo $result;
